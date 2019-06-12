0f618ba14936b748893ff09f17078524d2b6a523
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-12 15:55:32 +0200

    configure standard electronics-spa in bootcamp app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { translations } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com'
+        }
+      },
+      site: {
+        baseSite: 'electronics-spa'
+      },
+      i18n: {
+        resources: translations
+      }
+    }),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
