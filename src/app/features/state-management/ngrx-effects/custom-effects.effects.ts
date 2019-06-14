6f7f3f66621425efddd56bc094d8f1de32b08f1d
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-14 12:42:21 +0200

    fix prettier

--- src/app/features/state-management/ngrx-effects/custom-effects.effects.ts
+++ src/app/features/state-management/ngrx-effects/custom-effects.effects.ts
@@ -1,40 +1,37 @@
-import {Injectable} from '@angular/core';
-import {Actions, Effect, ofType} from '@ngrx/effects';
-import {Observable} from 'rxjs';
-import {map, tap} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import {
-    ADD_ENTRY_SUCCESS,
-    AddEntrySuccess,
-    AddMessage,
-    GlobalMessageType,
-    LOAD_PRODUCT_SUCCESS,
-    LoadProductSuccess,
-    Product
+  AddEntrySuccess,
+  AddMessage,
+  ADD_ENTRY_SUCCESS,
+  GlobalMessageType,
+  LoadProductSuccess,
+  LOAD_PRODUCT_SUCCESS,
+  Product,
 } from '@spartacus/core';
-
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class CustomEffectsEffects {
+  @Effect({ dispatch: false })
+  logProductLoading$: Observable<any> = this.actions$.pipe(
+    ofType(LOAD_PRODUCT_SUCCESS),
+    map((action: LoadProductSuccess) => action.payload.code)
+  );
 
-    @Effect({dispatch: false})
-    logProductLoading$: Observable<any> = this.actions$.pipe(
-        ofType(LOAD_PRODUCT_SUCCESS),
-        map((action: LoadProductSuccess) => action.payload.code),
-        tap(code => console.log('product loaded', code)));
+  @Effect()
+  addToCartNotify$: Observable<any> = this.actions$.pipe(
+    ofType(ADD_ENTRY_SUCCESS),
+    map((action: AddEntrySuccess) => action.payload.entry.product),
+    map(
+      (product: Product) =>
+        new AddMessage({
+          text: { raw: product.name + ' added to cart' },
+          type: GlobalMessageType.MSG_TYPE_CONFIRMATION,
+        })
+    )
+  );
 
-    @Effect()
-    addToCartNotify$: Observable<any> = this.actions$.pipe(
-        ofType(ADD_ENTRY_SUCCESS),
-        map((action: AddEntrySuccess) => action.payload.entry.product),
-        map((product: Product) => new AddMessage(
-            {
-                text: {raw: product.name + ' added to cart'},
-                type: GlobalMessageType.MSG_TYPE_CONFIRMATION
-            }
-        ))
-    );
-
-    constructor(private actions$: Actions) {
-    }
-
+  constructor(private actions$: Actions) {}
 }
