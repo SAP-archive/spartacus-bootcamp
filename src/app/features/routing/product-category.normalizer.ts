bd4e36f6e073d7f5b5655d3577fba83c37ff0ef6
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-14 09:06:23 +0200

    Add supplier and category to the product route

---
+++ src/app/features/routing/product-category.normalizer.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Converter, Occ, Product } from '@spartacus/core';
+
+@Injectable()
+export class ProductCategoryNormalizer
+  implements Converter<Occ.Product, Product> {
+  convert(source: Occ.Product, target?: any): Product {
+    if (target === undefined) {
+      target = { ...(source as any) };
+    }
+    if (source.categories && source.categories.length) {
+      target.category = source.categories[0].name; //
+    }
+
+    return target;
+  }
+}
--- src/app/features/routing/routing.module.ts
+++ src/app/features/routing/routing.module.ts
@@ -1,40 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ConfigModule, UrlModule } from '@spartacus/core';
+import { UrlModule } from '@spartacus/core';
 import { OutletRefModule } from '@spartacus/storefront';
-import { AppStaticLinkComponent } from './route-link.component';
-import { AppStaticPageComponent } from './static-page.component';
+import { CustomProductRoutesModule } from './product-routes.module';
+import { AppStaticPageComponent as AppStaticSalePageComponent } from './static-page.component';
 
 const routes: Routes = [
   {
-    path: 'static-page',
-    component: AppStaticPageComponent
-  }
+    path: 'sale',
+    component: AppStaticSalePageComponent,
+  },
 ];
 
 @NgModule({
   imports: [
     // standard non-spartacus routes
     RouterModule.forChild(routes),
-    // configurable routes
-    ConfigModule.withConfig({
-      routing: {
-        routes: {
-          product: {
-            paths: ['product-catalog/:productCode/:name', 'product-alias/:productCode'],
-            paramsMapping: { productCode: 'code' }
-          }
-        }
-      }
-    }),
 
     // semantic URLs are controlled
     UrlModule,
 
+    // demonstrate custom product route configurations
+    CustomProductRoutesModule,
+
     // use outlets to add a link to the header
-    OutletRefModule
+    OutletRefModule,
   ],
-  declarations: [AppStaticPageComponent, AppStaticLinkComponent],
-  exports: [RouterModule, AppStaticLinkComponent]
+  declarations: [AppStaticSalePageComponent],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
