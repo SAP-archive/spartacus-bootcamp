576caa742797927d2e7a1ee755866cd847f60224
dunqan <dunqan@w9.pl>
2019-06-16 19:57:32 +0200

    Extract wish list data service to decouple effect with loading logic

--- src/app/features/state/wish-list-state/wish-list.effects.ts
+++ src/app/features/state/wish-list-state/wish-list.effects.ts
@@ -7,8 +7,7 @@ import {
   WishListActions,
   WishListActionTypes,
 } from './wish-list.actions';
-
-const WISH_LIST_KEY = 'wish-list-data';
+import { WishListDataService } from './wish-list-data.service';
 
 @Injectable()
 export class WishListEffects {
@@ -16,7 +15,7 @@ export class WishListEffects {
   loadWishLists$ = this.actions$.pipe(
     ofType(WishListActionTypes.LoadWishLists),
     map(x => {
-      const wishList = this.getWishList();
+      const wishList = this.wishListData.getWishList();
       return new LoadWishListsSuccess(wishList);
     })
   );
@@ -25,7 +24,7 @@ export class WishListEffects {
   addWish$ = this.actions$.pipe(
     ofType(WishListActionTypes.AddWish),
     tap(action => {
-      this.addToWishList(action.payload);
+      this.wishListData.addToWishList(action.payload);
     })
   );
 
@@ -33,48 +32,12 @@ export class WishListEffects {
   removeWish$ = this.actions$.pipe(
     ofType(WishListActionTypes.RemoveWish),
     tap(action => {
-      this.removeFromWishList(action.payload);
+      this.wishListData.removeFromWishList(action.payload);
     })
   );
 
-  /**
-   * Below you'll find helper functions to load and store wish list in localStorage
-   * In real implementation you would probably want to store wish list on a backend
-   * per user basis.
-   */
-
-  private getWishList(): string[] {
-    if (this.winRef.localStorage) {
-      const data = this.winRef.localStorage.getItem(WISH_LIST_KEY);
-      if (data) {
-        try {
-          return JSON.parse(data);
-        } catch (e) {}
-      }
-    }
-    return [];
-  }
-
-  private setWishList(wishes: string[]) {
-    if (this.winRef.localStorage) {
-      this.winRef.localStorage.setItem(WISH_LIST_KEY, JSON.stringify(wishes));
-    }
-  }
-
-  private addToWishList(wish: string) {
-    const wishList = this.getWishList();
-    if (wishList.indexOf(wish) === -1) {
-      wishList.push(wish);
-      this.setWishList(wishList);
-    }
-  }
-
-  private removeFromWishList(wishToRemove: string) {
-    this.setWishList(this.getWishList().filter(wish => wish !== wishToRemove));
-  }
-
   constructor(
     private actions$: Actions<WishListActions>,
-    private winRef: WindowRef
+    private wishListData: WishListDataService
   ) {}
 }
