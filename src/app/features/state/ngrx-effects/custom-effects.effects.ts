8ee7cd5644158e82a24e9778513b27ff9864465a
dunqan <dunqan@w9.pl>
2019-06-16 14:07:17 +0200

    Add comments to ngrx-effects example

--- src/app/features/state/ngrx-effects/custom-effects.effects.ts
+++ src/app/features/state/ngrx-effects/custom-effects.effects.ts
@@ -10,22 +10,24 @@ import {
   Product,
 } from '@spartacus/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class CustomEffectsEffects {
-  @Effect({ dispatch: false })
+  @Effect({ dispatch: false }) // Use `{ dispatch: false }` for effects that don't emit any action
   logProductLoading$: Observable<any> = this.actions$.pipe(
-    ofType(LOAD_PRODUCT_SUCCESS),
-    map((action: LoadProductSuccess) => action.payload.code)
+    ofType(LOAD_PRODUCT_SUCCESS), // listen for successful product load
+    map((action: LoadProductSuccess) => action.payload.code), // extract product code from action payload
+    tap(code => console.log('product loaded', code)) // do something useful with product code
   );
 
   @Effect()
   addToCartNotify$: Observable<any> = this.actions$.pipe(
-    ofType(ADD_ENTRY_SUCCESS),
-    map((action: AddEntrySuccess) => action.payload.entry.product),
+    ofType(ADD_ENTRY_SUCCESS), // listen for successful add to cart
+    map((action: AddEntrySuccess) => action.payload.entry.product), // get product from action payload
     map(
       (product: Product) =>
+        // return/emit action that will add custom global message
         new AddMessage({
           text: { raw: product.name + ' added to cart' },
           type: GlobalMessageType.MSG_TYPE_CONFIRMATION,
