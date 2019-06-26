96e2d4f13b399aea3b2d8d66c0d161ca88113827
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-26 11:37:18 +0200

    Document multisite module

--- src/app/features/multi-site/multi-site.module.ts
+++ src/app/features/multi-site/multi-site.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ConfigModule } from '@spartacus/core';
 
+/**
+ * Demonstrates stateful routing parameters in the URL, by adding
+ * the site, language and currency to the URL.
+ *
+ * Please note that the site context model will change before 1.0.
+ */
 @NgModule({
   imports: [
     ConfigModule.withConfig({
