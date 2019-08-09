48ba70bf325eb1013afeecc692956b875752e909
dunqan <dunqan@w9.pl>
2019-08-09 13:41:27 +0200

    Fix breadcrumbs component

--- react-cms-components/src/components/breadcrumbs.js
+++ react-cms-components/src/components/breadcrumbs.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function Breadcrumbs({ routing }) {
   const [url, setUrl] = useState('');
